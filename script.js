4e5e2f4115a85535c9d4cd0fec74c6e00dbf67fa
kevin <kevinbarrios@ymail.com>
2021-10-25 16:59:11 -0500

    updated HTML

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelector("form").addEventListener("submit", (e) => {
+    e.preventDefault()
+
+
+{/* <div class="dropdown">
+  <button onclick="myFunction()" class="dropbtn">Dropdown</button>
+  <div id="myDropdown" class="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div> */}
