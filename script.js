3bf184f0c5870fa0f37f2a384de144740282c7d0
kevin <kevinbarrios@ymail.com>
2021-10-26 16:29:15 -0500

    changes

--- script.js
+++ script.js
@@ -2,3 +2,5 @@
 // document.addEventListener("DOMContentLoaded", () => {
 //     document.querySelector("form").addEventListener("submit", (e) => {
 //     e.preventDefault()
+
+const button = document.querySelector('.btn')
